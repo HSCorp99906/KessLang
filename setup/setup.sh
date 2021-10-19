ba23b7e5b9f5489fe2ecc417d5e64155741a72ee
Ian <teaqllabs@gmail.com>
2021-10-19 18:42:01 -0400

    Added setup utils and a README.

---
+++ setup/setup.sh
@@ -0,0 +1,12 @@
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run program as root."
+else
+    echo "Setting up.."
+    sleep 1
+    mv klcc /bin/
+    echo "Removing setup junk.."
+    sleep 1
+    echo "You may now delete the setup folder."
+    echo "Create a .kess file and run: klcc filename.kess"
+    rm setup.sh
