a4e1135e4b9d7f7db950b776fbdcb91caf771dc8
Ian <teaqllabs@gmail.com>
2021-10-25 09:25:14 -0400

    Restricted if statement

--- src/Core.cpp
+++ src/Core.cpp
@@ -1056,6 +1056,10 @@ void execute() {
                         }
 
                         pauseIfRead = true;
+
+                        if (std::regex_match(ifLine, std::regex("(out\\(\\w+\\d+\\);|int\\s*\\w+\\d*\\s*=\\s*\\d*;|str\\s*\\w+\\d*\\s*=\\s*\"{1}[A-Z0-9a-z]+\"{1})"))) {
+                            exit_err("RUNTIME ERROR: Using unsupported feature with if statements on line: " + std::to_string(ifLineNum));
+                        }
                     }
                 }
                 break;
